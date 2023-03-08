1f00d4e1e630148b5e86101394c3f10ab4f7a4d5
suthyscott <sutherlandscott21@gmail.com>
2023-03-08 09:51:59 -0700

    added associations and endpoints

---
+++ server/controller/profileCtrl.js
@@ -0,0 +1,5 @@
+module.exports = {
+    getProfile: async (req, res) => {
+        console.log('hit getProfile')
+    }
+}
--- server/index.js
+++ server/index.js
@@ -6,13 +6,25 @@ const {SERVER_PORT} = process.env
 const {sequelize} = require('./util/database')
 const {User} = require('./models/user')
 const {Profile} = require('./models/profile')
+const {register, login} = require('./controller/authCtrl')
+const {getProfile} = require('./controller/profileCtrl')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
+User.hasOne(Profile)
+Profile.belongsTo(User)
+
+app.post('/api/register',  register)
+app.post('/api/login', login)
+
+app.get('/api/profile', getProfile)
+
+
 sequelize.sync()
+// sequelize.sync({force: true})
     .then(res => {
         app.listen(SERVER_PORT, () => console.log(`Take us to warp ${SERVER_PORT}!`))
     })
