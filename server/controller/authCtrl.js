e2e9468ee748cd16d22a511a07c17528c8d505ad
suthyscott <sutherlandscott21@gmail.com>
2023-03-08 15:58:34 -0700

    added register function to controller

--- server/controller/authCtrl.js
+++ server/controller/authCtrl.js
@@ -1,7 +1,43 @@
+require('dotenv').config()
+const {SECRET} = process.env
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const {User} = require('../models/user')
+
+const createToken = (username,id) => {
+    return jwt.sign({username, id}, SECRET, {expiresIn: '2 days'})
+}
 
 module.exports = {
     register: async (req, res) => {
         console.log('hit register')
+        try{
+            const {username, password} = req.body
+            let foundUser = await User.findOne({where: {username}})
+            if(foundUser){
+                res.status(400).send('That username is already taken')
+            } else {
+                const salt = bcrypt.genSaltSync(5)
+                const hash = bcrypt.hashSync(password, salt)
+                const newUser = await User.create({
+                    username,
+                    hashedPass: hash
+                })
+                const token = createToken(newUser.dataValues.username, newUser.dataValues.id)
+                const exp = Date.now() + 1000 * 60 * 60 * 48
+
+                res.status(200).send({
+                    username: newUser.dataValues.username,
+                    userId: newUser.dataValues.id,
+                    token,
+                    exp
+                })
+            }
+
+        } catch(err) {
+            console.log(err)
+            res.status(400).send('Something broke')
+        }
     },
     login: async (req, res) => {
         console.log('hit login')
